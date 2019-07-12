cb75faabb386a0511cb12e01982e26fdeb80db35
Carlos H Marques da Cunha Filho <rickadt@gmail.com>
2019-07-12 08:19:55 -0300

    Desemvolvimento do sistema de pedidos   - add checagem de erros     - duplicados     - cooperados e vendedores não cadastrados     - pagina de erro   - listagem de pedidos com paginação     - a paginação precisa de melhora

--- app/blueprints/pedidos.py
+++ app/blueprints/pedidos.py
@@ -1,3 +1,4 @@
+from math import ceil
 from flask import Blueprint, render_template, request, jsonify
 from flask_security import login_required, current_user
 from app import db
@@ -5,6 +6,7 @@ from app import db
 from app.models import (Pedido, Cupon, Filial, Segmento, Coop, Vendedor,
                         RoleUser, Role)
 from sqlalchemy import text, distinct
+# from sqlalchemy.sql import func
 from app.utils import Log
 
 
@@ -38,37 +40,6 @@ def autocompletevendedor():
     return jsonify(matching_results=results)
 
 
-@pedidos_bp.route('/pedidos/new')
-@login_required
-def new():
-    user = current_user.get_id()
-    user_name = current_user.name
-
-    # Caso o usuário for do grupo 'filial_user', só pode lançar pedido para
-    # as filiais a qual pertence na tabela 'filiais_users' (FilialUser)
-    if current_user.roles[0].name == 'filial_user':
-        print('\n\n elaia')
-        filial_id = current_user.filiais[0].id
-        filiais = Filial.query.filter_by(id=filial_id).all()
-    else:
-        filiais = Filial.query.order_by(Filial.nome.asc()).all()
-
-    segmentos = Segmento.query.all()
-    return render_template('pedidos_form_add.html', filiais=filiais,
-                           segmentos=segmentos, user_name=user_name)
-
-
-@pedidos_bp.route('/pedidos/edit')
-@login_required
-def edit():
-    user = current_user.get_id()
-    user_name = current_user.name
-    if user == 'admin':
-        return render_template('pedido_edit.html', user_name=user_name)
-    else:
-        return render_template('acesso_negado.html')
-
-
 def gera_cupom(valor, segmento_id, pedido):
     " Gera os cupons para concorrer a sorteios"
     valor = float(valor.replace(',', '.'))
@@ -91,6 +62,38 @@ def gera_cupom(valor, segmento_id, pedido):
         return quantidade
 
 
+@pedidos_bp.route('/pedidos')
+def index():
+    " Lista os ultimos 20 pedidos e um total de pedidos e valores "
+    pedidos = Pedido.query.order_by(Pedido.id.desc()).limit(20).all()
+    pedidos_quant = Pedido.query.count()
+    pedidos_valor = db.session.query(db.func.sum(Pedido.valor)).first()
+    print('\n\n\n', pedidos)
+    return render_template("pedidos_index.html", pedidos=pedidos,
+                           pedidos_quant=pedidos_quant,
+                           pedidos_valor=pedidos_valor[0])
+
+
+@pedidos_bp.route('/pedidos/new')
+@login_required
+def new():
+    user = current_user.get_id()
+    user_name = current_user.name
+
+    # Caso o usuário for do grupo 'filial_user', só pode lançar pedido para
+    # as filiais a qual pertence na tabela 'filiais_users' (FilialUser)
+    if current_user.roles[0].name == 'filial_user':
+        print('\n\n elaia')
+        filial_id = current_user.filiais[0].id
+        filiais = Filial.query.filter_by(id=filial_id).all()
+    else:
+        filiais = Filial.query.order_by(Filial.nome.asc()).all()
+
+    segmentos = Segmento.query.all()
+    return render_template('pedidos_form_add.html', filiais=filiais,
+                           segmentos=segmentos, user_name=user_name)
+
+
 @pedidos_bp.route("/pedidos/new/save", methods=["GET", "POST"])
 def new_save():
     if request.method == "POST":
@@ -110,8 +113,26 @@ def new_save():
 
         cooperado = Coop.query.filter_by(nome=nomeCooperado).first()
         Log('func new_save: Coop.query: {}'.format(cooperado))
+        if cooperado is None:
+            Log('ERROR func new_save: Cooperado não cadastrado')
+            msg = {"error": "Cooperado não cadastrado",
+                   "data": (nomeCooperado,),
+                   "btn_type": "button",
+                   "btn_url": "",
+                   "btn_label": "Voltar"}
+            return render_template('pedidos_error.html', msg=msg)
+
         vendedor = Vendedor.query.filter_by(nome=nomeVendedor).first()
         Log('func new_save: Vendedor.query: {}'.format(vendedor))
+        if vendedor is None:
+            Log('ERROR func new_save: Vendedor não cadastrado')
+            msg = {"error": "Vendedor não cadastrado",
+                   "data": (nomeVendedor,),
+                   "btn_type": "button",
+                   "btn_url": "",
+                   "btn_label": "Voltar"}
+            return render_template('pedidos_error.html', msg=msg)
+
         filial = Filial.query.filter_by(id=filial_id).first()
         Log('func new_save: Filial.query: {}'.format(filial))
 
@@ -132,7 +153,16 @@ def new_save():
             filial=filial).first()
         Log('func new_save: checa pedido: {}'.format(checa))
         if checa is not None:
-            return 'o pedido já existe'
+            Log('ERROR func new_save: Pedido já cadastrado')
+            msg = {"error": "pedido já cadastrado (duplicado)",
+                   "data": ("Cooperado: {}".format(nomeCooperado),
+                            "Vendedor: {}".format(nomeVendedor),
+                            "Filial: {}".format(filial),
+                            "Valor: {}".format(valor)),
+                   "btn_type": "button",
+                   "btn_url": "",
+                   "btn_label": "Voltar"}
+            return render_template('pedidos_error.html', msg=msg)
             # return render_template('erro_pedido_duplicado.html',pedido=checa)
 
         # Incluindo o pedido no banco de dados usando SqlAlchemy
@@ -150,7 +180,8 @@ def new_save():
         db.session.commit()
         Log('func new_save: pedido add commit: {}'.format(pedido))
 
-        # Gerando cupons
+        # gerando cupons
+        # gera os cupons, e já adiciona no banco de dados e faz o commit
         cupons = gera_cupom(
             valor=valor, segmento_id=segmento_id, pedido=pedido)
         Log('func new_save: gera cupons: {}\n'.format(cupons))
@@ -161,4 +192,48 @@ def new_save():
         else:
             idpedido = pedido.id
 
-        return "Pedido: {}<br> Cupons: {}".format(pedido, cupons)
+        # return "Pedido: {}<br> Cupons: {}".format(pedido, cupons)
+        return render_template("pedidos_saved.html", idpedido=idpedido,
+                               cupons=cupons)
+
+
+@pedidos_bp.route('/pedidos/list')
+@pedidos_bp.route('/pedidos/list/<int:page>')
+def list(page=None):
+    " Lista todos os pedidos em paginas (paginate) "
+
+    # quantidade de linhas por pagina
+    PER_PAGE = 5
+
+    if not page:
+        page = 0
+
+    # pagina anterior
+    if page <= 1:
+        left = None
+    if page > 1:
+        left = page - 1
+
+    pedidos_quant = Pedido.query.count()
+    if pedidos_quant > PER_PAGE:
+        pages = range(1, ceil(pedidos_quant / PER_PAGE) + 1)
+    else:
+        pages = (1)
+
+    # proxima pagina
+    if len(pages) == page:
+        right = None
+    else:
+        right = page + 1
+
+    # pega os pedidos
+    if page >= 1:
+        pedidos = Pedido.query.order_by(Pedido.id.desc())\
+            .limit(PER_PAGE)\
+            .offset(PER_PAGE * page - PER_PAGE).all()
+    else:
+        pedidos = Pedido.query.order_by(
+            Pedido.id.desc()).limit(PER_PAGE).offset(0).all()
+    print('\n\n\n', pedidos, '\n\n')
+    return render_template("pedidos_list.html", pedidos=pedidos, page=page,
+                           left=left, right=right, pages=pages)
